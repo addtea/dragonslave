2b4603b9b7aece248cb92fb0fabb234e7ebe5f47
Eugene Che <eugeneyche@gmail.com>
2016-06-16 20:43:42 -0700

    Styles changes w/ rebase

--- inc/input_queue.hpp
+++ inc/input_queue.hpp
@@ -3,6 +3,7 @@
 
 namespace dragonslave {
 
+
 enum class InputType {
     KEYBOARD,
     MOUSE_POS,
@@ -10,24 +11,30 @@ enum class InputType {
     MOUSE_SCROLL,
 };
 
-struct InputEvent {
-    InputEvent(InputType type,
-               int action, char key, double x, double y)
-               : type(type), action(action), key(key), x(x), y(y) {}
+
+struct InputEvent 
+{
     InputType type;
     int action = 0;
     char key = 0;
     double x = 0, y = 0;
+
+    InputEvent(InputType type,
+               int action, char key, double x, double y)
+      : type(type), action(action), key(key), x(x), y(y) 
+    { }
 };
 
 
 class InputQueue
 {
-    public:
-    void register_input(InputType, int, char, double, double);
+public:
     InputEvent get_next_event() { InputEvent tmp = queue.front(); queue.pop(); return tmp; }
+    void register_input(InputType, int, char, double, double);
 
-    private:
+private:
     std::queue<InputEvent> queue;
 };
+
+
 }
